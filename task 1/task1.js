64868d0263cf8dbf9c3257fccb59e767a4b182e7
marlenlsomme <marlen.l.somme@gmail.com>
2026-03-11 11:04:17 +0100

    task 1.c

--- task 1/task1.js
+++ task 1/task1.js
@@ -10,3 +10,22 @@ function convert(){
     let result=TextToArray(array)
     document.getElementById("result").textContent=JSON.stringify(result)
 }
+
+//Task 1.c
+
+function SumNumbers(numbers){
+    let list=document.getElementById("sumList")
+    list.innerHTML=""
+    for(let i=0;i<numbers.length-1; i++){
+        let sum=numbers[i]+numbers[i+1]
+        let li=document.createElement('li')
+        li.textContent=numbers[i]+"+"+numbers[i+1]+"="+sum
+        list.appendChild(li)
+    }
+}
+
+function sums(){
+    let input=document.getElementById("numbersInput").value
+    let numbers=input.split(",").map(Number)
+    SumNumbers(numbers)
+}
