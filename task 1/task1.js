55144bac749e5d8b6313c588cec2cd970e9ef9a8
marlenlsomme <marlen.l.somme@gmail.com>
2026-03-11 10:40:31 +0100

    task 1.b

--- task 1/task1.js
+++ task 1/task1.js
@@ -1,1 +1,12 @@
 //Task 1. b
+
+function TextToArray (textlist){
+    return textlist.map(Number)
+}
+
+function convert(){
+    let input=document.getElementById("textInput").value
+    let array=JSON.parse(input)
+    let result=TextToArray(array)
+    document.getElementById("result").textContent=JSON.stringify(result)
+}
