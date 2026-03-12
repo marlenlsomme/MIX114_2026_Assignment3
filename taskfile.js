59e9c1ba5d9c59197c60d6d146dfcd625c59cda9
karolinebruvik <karolinebruvik02@gmail.com>
2026-03-12 17:09:36 +0000

    La til funksjonalitet for sjekkboks

---
+++ taskfile.js
@@ -0,0 +1,46 @@
+//Task 1. a
+function splitLines(text) {
+    return text.split("\n")
+}
+
+//Task 2 - To-do list
+function addItem() {
+
+    const inputElement = document.getElementById('userInput');
+    const inputValue = inputElement.value;
+
+    if (inputValue !=="") {
+
+        const container = document.getElementById('list');
+        
+        const newItem = document.createElement('li');
+
+        const checkbox = document.createElement('input');
+        checkbox.type = "checkbox";
+
+        checkbox.addEventListener("change", function () {
+            if (checkbox.checked) {
+                newItem.style.textDecoration = "line-through"
+            }
+            else {
+                newItem.style.textDecoration = "none"
+            }
+        });
+        
+        newItem.appendChild(checkbox);
+        newItem.append(" " + inputValue);
+        
+        container.appendChild(newItem);
+
+        inputElement.value = "";
+        }   
+    }
+
+function deleteItem() {
+    const element = document.getElementById('elementId');
+    if (element) {
+        element.remove();
+        console.log('Element with id "${elementId}" has been removed from the list.');
+    }
+}
+
